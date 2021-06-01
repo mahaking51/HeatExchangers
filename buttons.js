5b8046001d95e105cd14ceffaba62ad32862044e
mahaking51 <mahaking144@gmail.com>
2021-06-01 23:34:00 +0530

    added tubes model with arrows for connection

---
+++ buttons.js
@@ -0,0 +1,59 @@
+//draw circles with specified color ,radius and a number within
+function drawCircles(x,y,r,clr,fillclr,number){
+
+    ctx.beginPath();
+    ctx.strokeStyle=clr;
+    ctx.save();
+    ctx.globalAlpha = 0.4;
+    ctx.fillStyle=fillclr;
+    ctx.arc(x,y, r, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+    ctx.restore();
+    ctx.font = "20px Times";
+    ctx.fillText(number, x-5, y+5);    
+    ctx.closePath();
+}
+//distance between two points
+function disPoint(x1,y1,x2,y2){
+    var distanceX=Math.pow((x1-x2),2)
+    var distanceY=Math.pow((y1-y2),2)
+    return Math.sqrt(distanceX+distanceY);
+    
+}
+//drawing arrowheads
+function canvas_arrow(context, fromx, fromy, tox, toy) {
+    var headlen = 10; // length of head in pixels
+    var dx = tox - fromx;
+    var dy = toy - fromy;
+    var midx=(fromx+tox)/2
+    var midy=(fromy+toy)/2
+    var angle = Math.atan2(dy, dx);
+    context.beginPath();
+    context.moveTo(fromx, fromy);
+    context.lineTo(tox, toy);
+    context.moveTo(midx, midy);
+    context.lineTo(midx - headlen * Math.cos(angle - Math.PI / 6), midy - headlen * Math.sin(angle - Math.PI / 6));
+    context.moveTo(midx, midy);
+    context.lineTo(midx - headlen * Math.cos(angle + Math.PI / 6), midy - headlen * Math.sin(angle + Math.PI / 6));
+    context.stroke();
+  }
+  function searchForArray(haystack, needle){
+    var i, j, current;
+    for(i = 0; i < haystack.length; ++i){
+      if(needle.length === haystack[i].length){
+        current = haystack[i];
+        for(j = 0; j < needle.length && needle[j] === current[j]; ++j);
+        if(j === needle.length)
+          return i;
+      }
+    }
+    return -1;
+  }
+  function reverseArr(input) {
+    var ret = new Array;
+    for(var i = input.length-1; i >= 0; i--) {
+        ret.push(input[i]);
+    }
+    return ret;
+}
---
+++ draw.js
@@ -0,0 +1,103 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+
+var row;
+var col;
+var buttons=[];
+var order=[];
+var click=0;
+var route=[];
+document.getElementById('submit').addEventListener('click',function(evt){
+    evt.preventDefault();
+    buttons=[];
+    order=[]
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    row=parseInt(document.getElementById('row').value)
+    col=parseInt(document.getElementById('col').value)
+    // console.log(row,col);
+    drawButtons(row,col);
+})
+//drawing rows and columns of buttons
+function drawButtons(r,c){
+    var obj;
+    num=1
+    k=c+1
+    l=r+1
+    xpos=canvas.width/k 
+    ypos=canvas.height/l
+    
+    for (var i=0;i<r;i++){
+        for(var j=0;j<c;j++){
+            drawCircles((j+1)*xpos,(i+1)*ypos,25,"black","white",num)
+            obj={
+                val:num,
+                X:(j+1)*xpos,
+                Y:(i+1)*ypos,
+                clicked:false,
+                order:0
+            }
+            buttons.push(obj);
+            num++;
+        }
+    }
+}
+function redrawButton(){
+    for(var i=0;i<buttons.length;i++){
+        if(order.length>0 && buttons[i].val==order[0].val ){
+            drawCircles(buttons[i].X,buttons[i].Y,25,"black","red",buttons[i].val);
+        }
+        else{
+            drawCircles(buttons[i].X,buttons[i].Y,25,"black","white",buttons[i].val);
+        }
+    }
+    
+}
+function drawArrows(){
+    // console.log(route);
+    for(var j=0;j<route.length;j++){
+        for(var k=0;k<1;k++){
+            canvas_arrow(ctx,route[j][k].X,route[j][k].Y,route[j][k+1].X,route[j][k+1].Y)
+        }
+    }
+}
+function clickDetection(x,y){
+    // console.log(click);
+for(var i=0;i<buttons.length;i++){
+    
+    if(disPoint(buttons[i].X,buttons[i].Y,x,y)<=25){
+        click++
+
+        buttons[i].clicked=true;
+        order.push(buttons[i]);
+        if(click==2){
+            click=0;
+            revOrder=reverseArr(order)
+            if(searchForArray(route,order)==-1 && searchForArray(route,revOrder)==-1){
+                route.push(order)
+            }
+            else{
+                alert('wrong input')
+            }
+            order=[]    
+
+        }
+
+        if(click==2 && disPoint(order[0].X,order[0].Y,order[1].X,order[1].Y)<=25){
+            click=0;
+            order=[];
+            console.log(route);
+        }
+
+    }
+}
+console.log(order);
+console.log(route);
+
+}
+canvas.addEventListener('click',function(evt){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    clickDetection(evt.offsetX,evt.offsetY);   
+    // console.log(order);
+    redrawButton();
+    drawArrows()
+})
