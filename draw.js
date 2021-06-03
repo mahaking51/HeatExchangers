746b9a9c7cf4c49dd89fd9a693fba2284cb9090d
mahaking51 <mahaking144@gmail.com>
2021-06-04 00:59:37 +0530

    circles placed above arrows

--- draw.js
+++ draw.js
@@ -54,9 +54,23 @@ function redrawButton(){
 }
 function drawArrows(){
     // console.log(route);
+    var x1,y1,x2,y2,dx,dy,angle;
+    var rad=25;
     for(var j=0;j<route.length;j++){
         for(var k=0;k<1;k++){
-            canvas_arrow(ctx,route[j][k].X,route[j][k].Y,route[j][k+1].X,route[j][k+1].Y)
+            x1=route[j][k].X
+            y1=route[j][k].Y
+            x2=route[j][k+1].X
+            y2=route[j][k+1].Y;
+            dx=x2-x1;
+            dy=y2-y1;
+            angle= Math.atan2(dy, dx);
+            x1=x1+rad*Math.cos(angle);
+            y1=y1+rad*Math.sin(angle);
+            x2=x2-rad*Math.cos(angle);
+            y2=y2-rad*Math.sin(angle);
+            console.log(x1,y1,x2,y2,angle);
+            canvas_arrow(ctx,x1,y1,x2,y2)
         }
     }
 }
@@ -101,6 +115,6 @@ canvas.addEventListener('click',function(evt){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     clickDetection(evt.offsetX,evt.offsetY);   
     // console.log(order);
+    drawArrows();
     redrawButton();
-    drawArrows()
 })
